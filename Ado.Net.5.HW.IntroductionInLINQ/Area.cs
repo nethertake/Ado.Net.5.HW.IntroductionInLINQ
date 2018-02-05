db48d76fc805ac730229284f0b5118a63005e386
Timur <timur.samig@gmail.com>
2018-02-05 18:51:25 +0600

    Add project files.

---
+++ Ado.Net.5.HW.IntroductionInLINQ/Area.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ado.Net._5.HW.IntroductionInLINQ
+{
+    public class Area
+    {
+        public int AreaId { get; set; }
+
+        public int TypeArea { get; set; }
+        public string Name { get; set; }
+        public int ParentId { get; set; }
+        public bool NoSplit { get; set; }
+        public bool AssemblyArea { get; set; }
+        public string FullName { get; set; }
+        public bool MultipleOrders { get; set; }
+        public bool HiddenArea { get; set; }
+        public string IP { get; set; }
+        public int PavilionId { get; set; }
+        public int TypeId { get; set; }
+        public int OrderExecution { get; set; }
+        public int Dependence { get; set; }
+        public int WorkingPeople { get; set; }
+        public int ComponentTypeId { get; set; }
+        public int GroupId { get; set; }
+        public int Segment { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
